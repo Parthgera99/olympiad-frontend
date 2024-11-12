8594ae5c10e8dea458b0cd7690c9ae530481fdff
Anshul Jain <anshuljain8110@gmail.com>
2024-11-12 14:14:00 +0530

    Backend initialized

--- server/server.js
+++ server/server.js
@@ -1,1 +1,31 @@
-console.log('bnalo bhai. mujhe iska ghanta kuchh ni pta')
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const dotenv = require('dotenv');
+const connectDB = require('./connectDB');
+
+
+//middleware innitilization
+const app = express();
+dotenv.config();
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Connect to database
+connectDB();
+
+const SECRET_API_PATH = process.env.SECRET_API_PATH;
+const PORT = process.env.PORT || 5000;
+
+// fetching the blue print(s)
+const indexRoute = require('./Routes/index')
+app.use("/"+SECRET_API_PATH,indexRoute)
+
+app.get('*', (req, res) => {
+    res.status(404).json({ error: "Route not found" });
+});
+
+app.listen(PORT, () => {
+  console.log(`Application is listening on port ${PORT}`)
+})
